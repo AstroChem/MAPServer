de193a591c480573c6660967699cd15746b87cd1
Ian Czekala <iancze@gmail.com>
2020-04-06 17:11:42 -0700

    added cube view and lightbox to runs

--- app/main/views.py
+++ app/main/views.py
@@ -9,7 +9,7 @@ from flask import (
 )
 from . import main
 
-from sqlalchemy import select, join, and_, func
+from sqlalchemy import select, join, and_, func, literal, desc, asc
 from mapsdb import schema
 
 from app.db import get_db
@@ -48,7 +48,80 @@ def transitions():
         result = db.execute(s)
         transitions_list = result.fetchall()
 
-    return render_template("transitions.html", transitions=transitions_list)
+        s = select([schema.disks.c.disk_name, schema.disks.c.disk_id])
+        result = db.execute(s)
+        disk_pairs = result.fetchall()
+        # sort this list alphabetically by disk name
+        disk_pairs.sort(key=lambda tup: tup[0])
+
+        # create a mapping of (disk_id, transition_id) to (num_ms, num_runs)
+        # do this by a cross join of disks to transitions, which we accomplish by
+        # doing a join with onclause=True
+        # then, do an outer join with measurement sets and an outerjoin with runs
+        # we needed to specify the onclause, because for somereason it wouldn't take both
+        # automatically.
+        # then, do a group-by and count to reduce these.
+
+        # i think we need to split this into two group-by actions
+        # see: http://www.dein.fr/writing-a-subquery-with-sqlalchemy-core.html
+        # and https://www.tutorialspoint.com/sqlalchemy/sqlalchemy_core_using_aliases.htm
+        # and https://docs.sqlalchemy.org/en/13/core/tutorial.html#using-aliases-and-subqueries
+        # also sqlite text, ch 5, advanced techniques, subqueries
+
+        # first, create the list of all (disk_id, transition_id) as an alias
+
+        j = (
+            schema.disks.join(schema.transitions, onclause=literal(True))
+            .outerjoin(
+                schema.measurement_sets,
+                onclause=and_(
+                    (schema.disks.c.disk_id == schema.measurement_sets.c.disk_id),
+                    (
+                        schema.transitions.c.transition_id
+                        == schema.measurement_sets.c.transition_id
+                    ),
+                ),
+            )
+            .outerjoin(
+                schema.runs,
+                onclause=(
+                    schema.measurement_sets.c.measurement_set_id
+                    == schema.runs.c.measurement_set_id
+                ),
+            )
+        )
+
+        s = (
+            select(
+                [
+                    schema.disks.c.disk_id,
+                    schema.transitions.c.transition_id,
+                    func.count(schema.measurement_sets.c.measurement_set_id).label(
+                        "ms_count"
+                    ),
+                    func.count(schema.runs.c.run_id).label("run_count"),
+                ]
+            )
+            .select_from(j)
+            .group_by(schema.disks.c.disk_id, schema.transitions.c.transition_id)
+        )
+
+        # execute these two tables and join the result by disk_id, transition_id
+        result = db.execute(s)
+        # make this a dictionary indexed by disk_id, transition_id
+        disk_transition_dict = {}
+        for r in result:
+            disk_transition_dict[(r["disk_id"], r["transition_id"])] = (
+                r["ms_count"],
+                r["run_count"],
+            )
+
+    return render_template(
+        "transitions.html",
+        disk_pairs=disk_pairs,
+        transitions=transitions_list,
+        disk_transition_dict=disk_transition_dict,
+    )
 
 
 @main.route("/disks/")
@@ -263,10 +336,87 @@ def run(run_id):
                 color=next(colors),
             )
 
-    script, div = components(p)
+    bokeh_script, bokeh_plot_div = components(p)
+
+    # get the disk_parameters
+    db = get_db()
+    with db.begin():
+
+        s = select([schema.cube_types])
+        result = db.execute(s)
+        cube_types = result.fetchall()
+
+        # create a join with runs, disk_id, etc.
+        j = (
+            schema.runs.join(schema.measurement_sets)
+            .join(schema.disks)
+            .join(schema.transitions)
+        )
+
+        s = (
+            select(
+                [schema.runs, schema.disks, schema.measurement_sets, schema.transitions]
+            )
+            .where(schema.runs.c.run_id == run_id)
+            .select_from(j)
+        ).reduce_columns()
+        result = db.execute(s)
+        combo_params = result.first()
+
+        j = schema.runs.join(schema.parameters)
+        s = (
+            select([schema.parameters])
+            .select_from(j)
+            .where(schema.runs.c.run_id == run_id)
+        )
+        run_parameters = db.execute(s).first()
+
+        # get all cubes produced for this run
+        j = schema.cubes.join(schema.runs).join(schema.cube_types)
+        s = (
+            select([schema.cubes.c.cube_id, schema.cube_types.c.cube_type])
+            .select_from(j)
+            .where(schema.runs.c.run_id == run_id)
+        ).reduce_columns()
+        result = db.execute(s)
+        print(result.keys())
+        cubes_list = result.fetchall()
+        print("cubes_list", cubes_list)
+
+        # go through and produce a nested list of cube, cube_images
+
+        # for each cube, get all the images and image paths
+        j = (
+            schema.cubes.join(schema.runs)
+            .join(schema.cube_types)
+            .join(schema.cube_images)
+        )
+        s = (
+            select([schema.cube_images])
+            .select_from(j)
+            .where(
+                and_(
+                    (schema.cubes.c.cube_type_id == 0), (schema.runs.c.run_id == run_id)
+                )
+            )
+            .order_by(asc(schema.cube_images.c.frequency))
+        )
+
+        result = db.execute(s)
+        cube_images = result.fetchall()
+        image_paths = ["/" + cube_image["image_path"] for cube_image in cube_images]
+
+        print(image_paths)
 
     return render_template(
-        "run_id.html", run_id=run_id, script=script, bokeh_plot_div=div
+        "run_id.html",
+        cube_types=cube_types,
+        combo_params=combo_params,
+        run_parameters=run_parameters,
+        run_id=run_id,
+        image_paths=image_paths,
+        bokeh_script=bokeh_script,
+        bokeh_plot_div=bokeh_plot_div,
     )
 
 
@@ -299,8 +449,6 @@ def cube(cube_id):
 
 
 # this doesn't end in a slash because we are looking to mimic a filename
-@main.route("/imgs/<path:filename>")
+@main.route("/<path:filename>")
 def send_file(filename):
-    return send_from_directory(
-        "/Users/ianczekala/Documents/MAPS/NRAO/IMLup/C18O/", filename
-    )
+    return send_from_directory(current_app.config["MAPS_ROOT"], filename)

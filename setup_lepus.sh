ab9057304ee7ba76afd6c5c9f9dbea2fe56d2994
Ian Czekala <iancze@gmail.com>
2020-04-18 16:48:40 -0700

    removed setup source.

--- setup_lepus.sh
+++
@@ -1,5 +0,0 @@
-source venv/bin/activate
-export SQLALCHEMY_DATABASE_URI="sqlite:////Users/ian/Documents/Research/Disks/MAPS-LP/data/RML-products/MAPS/MAPS.db"
-export MAPS_ROOT="/Users/ian/Documents/Research/Disks/MAPS-LP/data/RML-products/MAPS/"
-export FLASK_APP=maps.py
-export FLASK_ENV=development
